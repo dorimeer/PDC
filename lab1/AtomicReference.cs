563cebac2b53fad33c2d1ae3deddb02b334d30cd
Dorime_er <42622826+dorimeer@users.noreply.github.com>
2021-12-28 13:58:35 +0200

    Add files via upload

---
+++ lab1/AtomicReference.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace lab1
+{
+  public class AtomicReference<T> where T : class
+        {
+            private object _lock = new object();
+            private T _value;
+            public AtomicReference(T value) => _value = value;
+            public void LockedUpdate(Func<T,T> updateFunc) 
+            {
+                lock(_lock)
+                {
+                    _value = updateFunc(_value);
+                }
+            }
+
+            public void Set(T value) => _value = value;
+            
+            public T Value
+            {
+                get
+                {
+                    object obj = _value;
+                    return (T)Thread.VolatileRead(ref obj);
+                }
+            }
+            
+            public void LockedAccess(Action<T> action)
+            {
+                lock(_lock)
+                {
+                    action(_value);
+                }
+            }
+            public TResult LockedAccess<TResult>(Func<T, TResult> accessAndReturnResult)
+            {
+                lock(_lock)
+                {
+                    return accessAndReturnResult(_value);
+                }
+            }
+            public TResult LockedAccessAndUpdate<TResult>(Func<T, (T, TResult)> accessAndUpdate)
+            {
+                lock(_lock)
+                {
+                    TResult result;
+                    (_value, result) = accessAndUpdate(_value);
+                    return result;
+                }
+            }
+            
+            public bool CompareAndExchange(T newValue, T oldValue)
+            {
+                return ReferenceEquals(Interlocked.CompareExchange(ref _value, newValue, oldValue), oldValue);
+            }
+        }
+    }
---
+++ lab1/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab1
+{
+    public class Helper<T>
+    {
+        public static T SelectRandomItem(List<T> list)
+        {
+            int size = list.Count;
+            int index = new Random().Next(size);
+            int counter = 0;
+
+            foreach (T item in list) {
+                if (counter == index) {
+                    return item;
+                }
+
+                counter++;
+            }
+
+            return default;
+        }
+    }
+}
